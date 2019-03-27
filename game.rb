fec81becffc0d518b4967e36e8e5fbe811f6f9b8
jamcry <jamcry@hotmail.com>
2019-03-27 23:20:02 +0300

    Completed main functionality

--- game.rb
+++ game.rb
@@ -11,22 +11,68 @@ def select_random(arr)
     arr.sample
 end
 
-def display(word)
-    length = word.length
-    puts "_ " * length, ""
+def display_secret_word(word_array)
+    word_array.each {|c| print c + " "}
+end
+
+def create_secret_word(word)
+    # Create an array of length of word and fill with underscores ("_")
+    Array.new(word.length) { "_" }
+end
+
+def check_user_guess(word, secret_array, guess, num_of_guesses)
+    match_found = false
+    word_array = word.split("")
+    word_array.each_with_index do |letter, index|
+        # If letter equals guess and not previously guessed. (MUST BE IMPROVED)
+        if letter == guess && !secret_array[index] != (letter)
+            secret_array[index] = letter
+            match_found = true
+        end
+    end
+    match_found
 end
 
 def get_user_guess
-    print " > Your Guess (A-Z): "
+    print(" Enter your guess: ")
     gets.chomp.upcase
 end
 
-def check_user_guess(word, guess)
- # check if guess exists in the secret word
+def check_winning(word, secret_word_array)
+    word.split("") == secret_word_array
 end
+
+def end_game(word, secret_word_array)
+    if check_winning(word, secret_word_array)
+        puts "YOU WIN"
+    else
+        print " You want to see the word ? "
+        choice = gets.chomp
+        puts choice == "y" ? word : "Goodbye!"
+    end
+end
+num_of_guesses = 12
+num_of_tries = 0
 dict = load_words(dict_filename)
 word = select_random(dict)
+secret_word_array = create_secret_word(word)
 puts word
-display(word)
+while num_of_guesses > 0
+    guess = get_user_guess
+    if check_user_guess(word, secret_word_array, guess, num_of_guesses)
+        puts " [✓] You found #{secret_word_array.count(guess)} matches!"
+        display_secret_word(secret_word_array)
+    else
+        num_of_guesses -= 1
+        puts " [X] No match for '#{guess}'. You have #{num_of_guesses} guesses left!"
+    end
 
-puts " > INPUT: #{get_user_guess}"
+    if check_winning(word, secret_word_array)
+        puts "", " [!] YOU WIN!"
+        puts " [!] You guessed the word in #{num_of_tries} tries!" 
+        break
+    end
+    num_of_tries += 1
+end
+
+end_game(word, secret_word_array)
